f876c6f1a2f58ebc38cb81d2648e89a1c8aa8d37
hazelhd20 <hazelhd121@gmail.com>
2025-10-05 13:50:28 -0600

    Integrar Tailwind localmente

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: [
+    './index.php',
+    './public/**/*.php',
+    './app/**/*.php',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          'Inter',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
+    },
+  },
+  darkMode: 'class',
+  plugins: [],
+};
